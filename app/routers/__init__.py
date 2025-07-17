37657c14d807c493e0244bc841f32bf9aad09d70
Aditya Padhi <adityapadhi1996@gmail.com>
2025-07-17 16:47:24 +0530

    Added aadhar CRUD

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from . import (mcp)
+from . import (mcp, aadhar)
 
 api_router = APIRouter(prefix="/api")
 
 api_router.include_router(mcp.router)
+api_router.include_router(aadhar.router)
